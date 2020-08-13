a008e8748a09b24db5f118bdb0bff369dbdd2dbc
kk <kk@kk.com>
2020-08-13 10:41:54 +0800

    更新配置文件

--- application/config/site_info.php
+++ application/config/site_info.php
@@ -4,13 +4,13 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$config['site_name'] = '33号铺';
+$config['site_name'] = '煲汤师傅金城武店铺';
 
 
 //本app_id和app_secret仅做测试用途
 //请去淘宝开放平台申请自己的应用key和secret
 //http://open.taobao.com/index.htm
-$config['appkey'] = '12673864';
+/*$config['appkey'] = '12673864';
 $config['secretkey'] = 'd4cb57d3eec5d6f1cbbfd0f430caf282';
 
 //如果你处于防火墙之中，需要配置HTTP代理，请配置你的HTTP代理服务器地址和IP
@@ -22,4 +22,23 @@ $config['http_proxy'] = '';
 //SEO
 //关键词列表用英文逗号隔开
 $config['site_keyword'] = '淘宝,男装,裤子,33号铺';
-$config['site_description'] = '33号铺是一个简单好用的淘宝客瀑布流发布、管理系统。';
+$config['site_description'] = '33号铺是一个简单好用的淘宝客瀑布流发布、管理系统。';*/
+
+$config['site_keyword'] = '拼多多优惠券,男装,裤子,煲汤师傅金城武店铺';
+$config['site_description'] = '煲汤师傅金城武店铺是一个简单好用的拼多多优惠券网站、通过人工筛选为用户寻找出可以信的过的特特价商品。';
+
+//本app_id和app_secret仅做测试用途
+//请去淘宝开放平台申请自己的应用key和secret
+//http://open.taobao.com/index.htm
+$config['appkey'] = '12673864';
+$config['secretkey'] = 'd867b662986a488e81ddc211f25e5b89';
+
+//拼多多 key 和 secret
+$config['pdd_client_id'] = '123456';
+$config['pdd_client_secret'] = '123456';
+
+//SEO
+//关键词列表用英文逗号隔开
+$config['site_keyword'] = '拼多多优惠券';
+$config['site_description'] = '多多进宝瀑布流发布、管理系统。';
+
