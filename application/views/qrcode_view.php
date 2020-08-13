729fc3e206cf1fbf7d4eea9d287151d94f24e1a6
kk <kk@kk.com>
2020-08-13 10:04:05 +0800

    增加二维码展示页面

---
+++ application/views/qrcode_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="zh-CN">
+<head>
+<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url()?>assets/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url()?>assets/index.css?d=20120705" />
+	<!--[if lt IE 9]>
+	<script src="<?php echo base_url()?>assets/js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<header id="branding" role="banner">
+</header>
+
+<div>
+    <img src="<?php echo $date_uri ?>">
+</div>
+
+
+</body>
+</html>
